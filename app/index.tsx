4549a4343e0373af5fd9ebcb9d42cfe4be344dc9
Bishwash <drewwbishwash@gmail.com>
2025-07-31 19:40:06 +0545

    routing

--- app/index.tsx
+++ app/index.tsx
@@ -3,15 +3,24 @@ import { Redirect } from "expo-router";
 import { useAuthStore, User } from "@/hooks/useAuthstore";
 
 const App = () => {
+  // Uncomment this when using real auth state
   // const { user } = useAuthStore();
+
   const user = {
     fullName: "Bishwash Chaudhari",
   };
 
   if (user) {
-    return <Redirect href={"/home"} />;
+    return (
+      <Redirect
+        href={{
+          pathname: "/(root)/product/product",
+          params: { id: "1" },
+        }}
+      />
+    );
   } else {
-    return <Redirect href={"/log-in"} />;
+    return <Redirect href="/log-in" />;
   }
 };
 
