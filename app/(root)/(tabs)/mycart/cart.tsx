947f6bd5865f43b128f6aec7141643de3d3cdc89
Bishwash <drewwbishwash@gmail.com>
2025-08-02 23:05:31 +0545

    empty list component

--- app/(root)/(tabs)/mycart/cart.tsx
+++ app/(root)/(tabs)/mycart/cart.tsx
@@ -2,7 +2,11 @@ import { View, Text, FlatList, TouchableOpacity } from "react-native";
 import React from "react";
 import products from "@/data";
 import { Icon } from "@/components/ui/icon";
-import { ChevronLeft } from "lucide-react-native";
+import {
+  ChevronLeft,
+  EqualApproximatelyIcon,
+  ShoppingCart,
+} from "lucide-react-native";
 import { useRouter } from "expo-router";
 import { Button, ButtonText } from "@/components/ui/button";
 import CartItem from "@/components/cart/CartItem";
@@ -10,11 +14,13 @@ import CartItem from "@/components/cart/CartItem";
 const Cart = () => {
   const router = useRouter();
 
+  const userCart = [];
+
   return (
     <View className="flex-1 bg-white dark:bg-black px-8 pt-12 relative">
       {/* FlatList with padding to avoid being hidden behind footer */}
       <FlatList
-        data={products}
+        data={userCart}
         keyExtractor={(item) => item.id}
         renderItem={({ item }) => (
           <CartItem
@@ -27,6 +33,14 @@ const Cart = () => {
             imageUri={item.imageUri || require("@/assets/images/ui/image1.png")}
           />
         )}
+        ListEmptyComponent={
+          <View className="h-full items-center justify-center flex-auto">
+            <Icon as={ShoppingCart} size="xl" className="text-primary mb-4" />
+            <Text className="text-lg font-medium text-primary-400">
+              Oops! Nothing here yet.
+            </Text>
+          </View>
+        }
         showsVerticalScrollIndicator={false}
         contentContainerStyle={{ paddingBottom: 200 }} // prevents content from hiding behind footer
         ListHeaderComponent={() => (
@@ -48,30 +62,35 @@ const Cart = () => {
       />
 
       {/* Sticky footer */}
-      <View className="absolute bottom-0 left-0 right-0 px-8 pb-8 pt-6 bg-white dark:bg-black rounded-t-2xl border-slate-100/20 border-t-2">
-        <View className="flex flex-col gap-2">
-          <View className="flex flex-row items-center justify-between">
-            <Text className="text- font-light">Product Price</Text>
-            <Text className="font-light text-sm">$100</Text>
-          </View>
 
-          <View className="flex flex-row items-center justify-between">
-            <Text className="text font-light">Shipping cost</Text>
-            <Text className="font-light text-sm">Free</Text>
-          </View>
+      {userCart.length > 0 && (
+        <View className="absolute bottom-0 left-0 right-0 px-8 pb-8 pt-6 bg-white dark:bg-black rounded-t-2xl border-slate-100/20 border-t-2">
+          <View className="flex flex-col gap-2">
+            <View className="flex flex-row items-center justify-between">
+              <Text className="text- font-light">Product Price</Text>
+              <Text className="font-light text-sm">$100</Text>
+            </View>
 
-          <View className="flex flex-row items-center justify-between">
-            <Text className="text-xl font-medium">Subtotal</Text>
-            <Text className="font-medium text-lg">$100</Text>
-          </View>
+            <View className="flex flex-row items-center justify-between">
+              <Text className="text font-light">Shipping cost</Text>
+              <Text className="font-light text-sm">Free</Text>
+            </View>
 
-          <Button className="rounded-3xl h-14 mt-2" onPress={() => {}}>
-            <ButtonText onPress={() => router.push("/(root)/checkout")}>
-              Proceed to checkout
-            </ButtonText>
-          </Button>
+            <View className="flex flex-row items-center justify-between">
+              <Text className="text-xl font-medium">Subtotal</Text>
+              <Text className="font-medium text-lg">$100</Text>
+            </View>
+
+            <Button className="rounded-3xl h-14 mt-2" onPress={() => {}}>
+              <ButtonText
+                onPress={() => router.push("/(root)/checkout/shipping")}
+              >
+                Proceed to checkout
+              </ButtonText>
+            </Button>
+          </View>
         </View>
-      </View>
+      )}
     </View>
   );
 };
